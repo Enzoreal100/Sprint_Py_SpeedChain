03c330c9a12a539b39f907787db6c5f88e184804
Gabriel <150721374+ghpadula@users.noreply.github.com>
2024-06-16 14:45:46 -0300

    Mudança na funcao de calculo-nft, e funcao posicao

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 def meu_index (lista,elemento):
     for i in range(len(lista)):
         if elemento == lista[i]:
-            return i
+             return i
     
 
 def maior(lista):
@@ -23,44 +23,18 @@ def menor (lista):
             menor = lista[i]
     return menor
 
-def calculo_ntf(preco,posicao):
+def calculo_ntf(preco, posicao2):
+    percentuais = [0.15, 0.12, 0.09, 0.06, 0.03, 0.0, -0.03, -0.06, -0.09, -0.12, -0.15]
     preco2 = []
-    for i in posicao:
-        match i:
-            case 1:
-                valor = preco[meu_index(posicao,1)]
-                valor = valor + (valor * .15)
-                #print(valor)
-                preco2.append(valor)
-                #preco2[] = valor
-            case 2:
-                valor = preco[meu_index(posicao,2)]
-                valor = valor + (valor * .12)
-            # print(valor)
-                preco2.append(valor)
-            # preco[] = valor
-            case 3:
-                valor = preco[meu_index(posicao,3)]
-                valor = valor + (valor * .09)
-                preco[meu_index(posicao,3)] = valor
-                #print(valor)
-                preco2.append(valor)
-            case 4:
-                valor = preco[meu_index(posicao,4)]
-                valor = valor + (valor * .06)
-                preco[meu_index(posicao,4)] = valor
-                #print(valor)
-                preco2.append(valor)
-            case 5:
-                valor = preco[meu_index(posicao,5)]
-                valor = valor + (valor * .03)
-                preco[meu_index(posicao,5)] = valor
-            # print(valor)
-                preco2.append(valor)
-            case 6:
-                valor = preco[meu_index(posicao,6)]
-                #print(valor)
-                preco2.append(valor)
+    
+    for i in range(len(posicao2)):
+        pos = posicao2[i]
+        valor = preco[i]
+        if 1 <= pos <= 11:          
+            valor = valor + (valor * percentuais[pos - 1])
+        
+        preco2.append(valor)
+    
     return preco2
 
 def forca_escolha(lista,msg):
@@ -82,29 +56,34 @@ def sair_continuar():
         return False
     
     
-def posicao(piloto, i):
+def posicao(piloto, i, posicoes_usadas):#verifica a posicao que o usuario colocou em cada equipe para nao se repitir e ficar entre 1 e 11
     while True:
-        position = input(f"Digite a posição do piloto {piloto[i]} \n -->")
+        position = input(f"Digite a posição do piloto {piloto[i]} \n --> ")
         if position.isnumeric():
             position = int(position)
-            if 1 <= position <= 6:
-                return position
+            if 1 <= position <= 11:
+                if position not in posicoes_usadas:
+                    posicoes_usadas.append(position)
+                    return position
+                else:
+                    print("Esta posição já foi usada. Por favor, digite uma posição diferente.")
             else:
-                print("Digite um valor entre 1 e 6")
+                print("Digite um valor entre 1 e 11.")
         else:
             print("Entrada inválida. Digite um número.")
-            
 
 #Construção do código
 opcoes_sair = ["1","2","3"]
-posicao_nova = []
-piloto = ["Piloto1","Piloto2","Piloto3","Piloto4","Piloto5","Piloto6",]
-posiçao = [5,4,3,1,2,6]
-valor = [10,20,30,40,50,100]
+#posicao_nova = []
+piloto = ["Equipe1","Equipe2","Equipe3","Equipe4","Equipe5","Equipe6","Equipe7","Equipe8","Equipe9","Equipe10","Equipe11",]
+posiçao = []
+posicoes_usadas = []
+valor = [10,20,30,40,50,100,12,15,22,77,88]
 
-print("Bem-vindo!")
+
 while True:
-    escolha = forca_escolha(opcoes_sair, "Digite: \n [1] para ver os preços de nft de cada piloto \n [2] Para colocar a posição de cada piloto na mais nova corrida \n [3] Para sair\n -->")
+    print("Bem-vindo!")
+    escolha = forca_escolha(opcoes_sair, "Digite [1] para ver os preços de nft de cada Equipe \n [2] Para colocar a posição de cada Equipe na mais nova corrida \n [3] Para sair\n -->")
     if escolha == "1":
         for i in range(len(piloto)):
             print(f"O valor do piloto {piloto[i]} é de R${valor[i]}")
@@ -115,11 +94,11 @@ while True:
             break
     if escolha == "2":
         for i in range(len(piloto)):
-            position = posicao(piloto, i)
-            posicao_nova.append(position)
+            position = int(posicao(piloto, i, posicoes_usadas))
+            posiçao.append(position)
             print(f"Posição do {piloto[i]}: {position}")       
        
-        novos_valores = calculo_ntf(valor, posicao_nova)
+        novos_valores = calculo_ntf(valor, posiçao)
         
         for i in range(len(piloto)):
             print(f"Piloto: {piloto[i]}")
